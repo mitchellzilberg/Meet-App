fa8fc9a46ba59e9d1fe11d821e7c046540ee84e4
Mitch Zilberg2 <mitchell@Mitchells-MacBook-Pro.local>
2021-01-23 19:07:38 -0500

    number of events

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+// import { ErrorAlert } from "./Alert";
+
+class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 32,
+  };
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.props.updateEvents(null, value);
+    this.setState({
+      numberOfEvents: value,
+    });
+
+    if (value < 1) {
+      this.setState({
+        infoText: "Please choose a number of events between 1 and 32",
+      });
+    } else {
+      this.setState({
+        infoText: "",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="number-of-events">
+        <ErrorAlert text={this.state.infoText} />
+        <label className="number-of-events-label">Number of Events: </label>
+        <input
+          type="number"
+          className="event-number"
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChanged}
+        ></input>
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
