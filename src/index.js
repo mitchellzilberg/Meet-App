0772044745575e1840bcc4c21810cdfd7c94c128
Mitch Zilberg2 <mitchell@Mitchells-MacBook-Pro.local>
2021-01-24 12:24:08 -0500

    atatus test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+// import * as atatus from 'atatus-js';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +14,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+serviceWorker.unregister();
+atatus.config('259ee5e9bb574eb9b4abee9ee76ecacc').install();
+
