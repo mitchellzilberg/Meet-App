1ad50b5368522d86e8e0498d1de9cb2a7e2bad8e
Mitch Zilberg2 <mitchell@Mitchells-MacBook-Pro.local>
2021-01-27 14:03:09 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
 import { extractLocations, getEvents } from "./api";
 import { OfflineAlert } from "./Alert";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import EventGenre from "./EventGenre";
 
 class App extends Component {
   state = {
@@ -85,20 +95,31 @@ class App extends Component {
   };
 
   render() {
+    // const { locations, numberOfEvents, events } = this.state;
     return (
       <div className="App">
         <h1>Meet-App</h1>
         <OfflineAlert text={this.state.alertText} />
-        <NumberOfEvents
-          numberOfEvents={this.state.numberOfEvents}
-          updateEvents={this.updateEvents}
-        />
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
         />
+        <NumberOfEvents
+          numberOfEvents={this.state.numberOfEvents}
+          updateEvents={this.updateEvents}
+        />
+        <h4>Events in each city</h4>
         <div className="data-vis-wrapper">
-          <h4>Events in each city</h4>
+          <EventGenre events={this.state.events}/>
+          <ResponsiveContainer height={400}>
+            <ScatterChart margin={{top: 20, right: 20, bottom: 20, left: 20,}}>
+              <CartesianGrid />
+              <XAxis type='category' dataKey='city' name='city' />
+              <YAxis type='number' dataKey='number' name='number of events' allowDecimals={false} />
+              <Tooltip cursor={{strokeDasharray: '3 3'}} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
         </div>
         <EventList events={this.state.events} />
       </div>
--- src/index.js
+++ src/index.js
@@ -7,20 +7,11 @@ import * as serviceWorker from './serviceWorker';
 // import * as atatus from 'atatus-js';
 import * as atatus from 'atatus-spa';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 serviceWorker.register();
-// serverWorker.unregister();
+// serviceWorker.unregister();
 atatus.config('259ee5e9bb574eb9b4abee9ee76ecacc').install();
 
